a586f16f5fa7ca51b8da0b1a25041ae375148c3d
gollarajeswari27 <gollarajeswari70@gmail.com>
2025-11-15 18:54:02 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Nov 14, 2025',
+  birthdate: 'Nov 16, 2025',
   name: 'Triveni'
 };
 
